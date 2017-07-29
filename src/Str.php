78baec8be207023e3c72efab8a2103f646870657
e200 <eleandro@inbox.ru>
2017-07-29 22:23:21 +0100

    First commit

---
+++ src/Str.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Unity\Helpers;
+
+class Str
+{
+    static function contains($needle, $source)
+    {
+        return strpos($source, $needle) !== false;
+    }
+
+    static function startsWith($needle, $source)
+    {
+        return strpos($source, $needle) == 0;
+    }
+
+    static function endsWIth($needle, $source)
+    {
+        $sourceLength = strlen($source);
+
+        return strpos($source, $needle) == ($sourceLength - 1);
+    }
+}
---
+++ tests/StrTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Unity\Helpers\Str;
+use PHPUnit\Framework\TestCase;
+
+class StrTest extends TestCase
+{
+    function testContains()
+    {
+        $this->assertEquals(true, Str::Contains('@', '@'));
+    }
+
+    function testNotContain()
+    {
+        $this->assertEquals(false, Str::Contains('$', '@'));
+    }
+
+    function testStartsWith()
+    {
+        $this->assertEquals(true, Str::startsWith('@', '@'));
+        $this->assertEquals(true, Str::startsWith('@', '@_'));
+    }
+
+    function testNotStartWith()
+    {
+        $this->assertEquals(false, Str::startsWith('@', '_@'));
+    }
+
+    function testEndsWith()
+    {
+        $this->assertEquals(true, Str::endsWIth('@', '@'));
+        $this->assertEquals(true, Str::endsWIth('@', '_@'));
+    }
+
+    function testNotEndsWith()
+    {
+        $this->assertEquals(false, Str::endsWIth('@', '@_'));
+    }
+}
