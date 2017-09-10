71514f1cf5df6eea3490dedc6ba56784eebcca67
e200 <eleandro@inbox.ru>
2017-09-10 20:27:00 +0100

    Added FileTest

---
+++ tests/FileTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Unity\Support\File;
+
+class FileTest extends TestCase
+{
+    protected $folder;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $dir = [
+            'file.php' => ''
+        ];
+
+        $virtualFolder = \org\bovigo\vfs\vfsStream::setup('tmp', 444, $dir);
+
+        $this->folder = $virtualFolder->url();
+    }
+
+    function testExists()
+    {
+        $this->assertTrue(File::exists($this->folder));
+        $this->assertFalse(File::exists(null));
+    }
+
+    function testIsFile()
+    {
+        $file = $this->folder . DIRECTORY_SEPARATOR . 'file.php';
+
+        $this->assertTrue(File::isFile($file));
+        $this->assertFalse(File::isFile(null));
+    }
+
+    function testIsDir()
+    {
+        $this->assertTrue(File::isDir($this->folder));
+        $this->assertFalse(File::isDir(null));
+    }
+}
