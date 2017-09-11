7ad60b22898bc523616ee5eba19f1cbafb253ae9
e200 <eleandro@inbox.ru>
2017-09-11 19:26:01 +0100

    Added tests for 1.2.2 updates

--- tests/ArrTest.php
+++ tests/ArrTest.php
@@ -5,7 +5,7 @@ use Unity\Support\Arr;
 
 class ArrTest extends TestCase
 {
-    public function testnestedSearch()
+    public function testNestedGetHas()
     {
         $array = [
             'database' => [
@@ -19,7 +19,8 @@ class ArrTest extends TestCase
 
         $keys = ['database', 'users', 'eleandro', 'password'];
 
-        $this->assertEquals('e200', Arr::nestedSearch($array, $keys));
+        $this->assertEquals('e200', Arr::nestedGet($keys, $array));
+        $this->assertTrue(Arr::nestedHas($keys, $array));
     }
 
     public function testMerge()
